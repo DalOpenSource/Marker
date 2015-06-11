0ccceec35c2b3366001131b8692282d531af29c0
Alex Safatli <safatli@cs.dal.ca>
2015-06-11 13:15:06 -0300

    Working on unit tests.

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
+  fixtures :users, :courses
   # test "the truth" do
   #   assert true
   # end
+  test "query" do
+  	assert_not_nil david.roles.find_by_course(one).title
+  end
 end
