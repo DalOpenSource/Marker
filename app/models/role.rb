a99bb6bbbaf884e362d294c1f0d5a3b76ac69178
Alex Safatli <safatli@cs.dal.ca>
2015-06-11 12:50:00 -0300

    Changed standing to title in Role.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,12 +1,12 @@
 class Role < ActiveRecord::Base
   # a user (could) have many roles in a course.
   # a course has many users with different roles.
-  enum standing: [:student, :marker, :instructor]
+  enum title: [:student, :marker, :instructor]
   belongs_to :course
   belongs_to :user
-  validates :standing, :presence: true, :inclusion: { in: standing }
-  validates :course_id, :presence: true
-  validates :user_id, :presence: true
+  validates :title, :presence: true, :numericality: true, :inclusion: { in: title }
+  validates :course_id, :presence: true, :numericality: true
+  validates :user_id, :presence: true, :numericality: true
   validates_associated :users
   validates_associated :courses
 end
