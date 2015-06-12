7aed2f6aa608fa086f227b571cba413c543a734a
Alex Safatli <safatli@cs.dal.ca>
2015-06-12 17:32:24 -0300

    Cleaned up migrations.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   enum semester: [:winter, :summer, :fall]
   validates :prefix, presence: true, format: { with: /\A[A-Z]+\z/, 
   	message: "needs uppercase letters" }
-  validates :code, presence: true, numericality: true
+  validates :code, presence: true, inclusion: { in: 100..9999 }, numericality: true
   validates :name, format: { with: /\A\w+\z/, message: "not a proper name" }
   validates :year, presence: true, inclusion: { in: 1900..Date.today.year }, 
   	format: { with: /(19|20)\d{2}/i, message: "should be a four-digit year" }
--- db/migrate/20150611145301_create_roles.rb
+++ db/migrate/20150611145301_create_roles.rb
@@ -1,9 +1,9 @@
 class CreateRoles < ActiveRecord::Migration
   def change
     create_table :roles do |t|
-      t.integer :title # %(:student :marker :instructor)
-      t.integer :course_id
-      t.integer :user_id
+      t.integer :title, default: :student # %(:student :marker :instructor), default a student
+      t.belongs_to :course # generate foreign key course_id
+      t.belongs_to :user # generate foreign key user_id
       t.timestamps null: false
     end
     add_index :roles, :course_id
