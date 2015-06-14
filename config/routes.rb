4454b28bc45382346acd9c2627e4f58598b58096
Alex Safatli <safatli@cs.dal.ca>
2015-06-14 10:50:27 -0300

    Progress Issue #5

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'users/:id' => 'users#show', as: :user
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
