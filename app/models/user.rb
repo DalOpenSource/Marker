0712138bfe37593723b5b8e037079641cfb70c97
Alex Safatli <safatli@cs.dal.ca>
2015-06-22 17:49:58 -0300

    Added shortcut methods to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,46 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :validatable, :authentication_keys => [:username]
   has_many :permissions
   has_many :courses, :through => :permissions
   validates :username, presence: true, uniqueness: { :case_sensitive => false }
+
+  # Public Methods
+  def in_course?(course)
+    course_permissions(course) != nil
+  end
+
+  def attends?(course)
+    c_permissions = course_permissions(course)
+    unless c_permissions == nil
+      return c_permissions.attends
+    end
+    false
+  end
+
+  def can_mark?(course)
+    c_permissions = course_permissions(course)
+    unless c_permissions == nil
+      return c_permissions.marks
+    end
+    false
+  end
+
+  def can_edit?(course)
+    c_permissions = course_permissions(course)
+    unless c_permissions == nil
+      return c_permissions.edits
+    end
+    false
+  end
+
+  # Private Methods
+  private
+
+  def course_permissions(course)
+    permissions.find_by(course: course)
+  end
+
 end
