ed3f1adb0e5d05b2ef11016accaa21c629d36a07
Alex Safatli <safatli@cs.dal.ca>
2015-06-27 13:57:45 -0300

    Started modifying routes for profile editing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
+
   before_filter :authenticate_user!
 
-  def show
-    @user = User.find(params[:id])
-    @courses = @user.courses
-    @permissions = @user.permissions
+  def edit
+    @user = current_user
+  end
+
+  def update_info
+    @user = User.find(current_user.id)
+    if @user.update_with_password(params.require(:user).permit(:password,:password_confirmation,:current_password))
+      # Sign in the user by passing validation in case their password changed
+      sign_in @user, :bypass => true
+      redirect_to root_path
+    else
+      render "edit"
+    end
   end
 
 end
