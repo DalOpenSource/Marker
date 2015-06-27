7acd26a69f656f76d63b30bc17a3566ff01f5522
Alex Safatli <safatli@cs.dal.ca>
2015-06-27 11:24:16 -0300

    Removing profile for all user for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'courses#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-  get 'users/:id' => 'users#show', as: :user
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
