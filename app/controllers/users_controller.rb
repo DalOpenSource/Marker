40ac552cdd921311fd32b77dca4a66da38e3ddd0
Alex Safatli <safatli@cs.dal.ca>
2015-06-28 11:17:08 -0300

    Added bootstrap class helper and fixed up edit profile screen.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def update
+    @user = current_user
+    @user.update_without_password(user_params)
+    redirect_to :back, notice: "User updated successfully."
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
 end
